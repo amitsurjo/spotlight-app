1ccfba0f9961d9c711755955d918dd0af6f919b5
amitsurjo <amitgoldersurjo@gmail.com>
2025-09-09 05:54:10 +0600

    setup clerk, add .env file, setup cache.ts file, build a login page and add handle google signin function,add components to setup authentication functionality

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,27 @@
-import { Stack } from "expo-router";
+import InitialLayout from "@/components/InitialLayout";
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+  throw new Error(
+    "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+  );
+}
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-        <Stack screenOptions={{ headerShown: false }} />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ClerkLoaded>
+        <SafeAreaProvider>
+          <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+            <InitialLayout />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
