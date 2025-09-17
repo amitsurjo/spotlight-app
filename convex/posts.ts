7dc2621b457fcb2f4cd560a9ad60ddfc4ebe2d9e
amitsurjo <amitgoldersurjo@gmail.com>
2025-09-17 07:17:47 +0600

    implement own profile screen

--- convex/posts.ts
+++ convex/posts.ts
@@ -167,7 +167,6 @@ export const deletePost = mutation({
       await ctx.db.delete(bookmark._id);
     }
 
-    //delete associated notifications
     const notifications = await ctx.db
       .query("notifications")
       .withIndex("by_post", (q) => q.eq("postId", args.postId))
@@ -187,5 +186,25 @@ export const deletePost = mutation({
     await ctx.db.patch(currentUser._id, {
       posts: Math.max(0, (currentUser.posts || 1) - 1),
     });
+  },
+});
+
+export const getPostsByUser = query({
+  args: {
+    userId: v.optional(v.id("users")),
+  },
+  handler: async (ctx, args) => {
+    const user = args.userId
+      ? await ctx.db.get(args.userId)
+      : await getAuthenticatedUser(ctx);
+
+    if (!user) throw new Error("User not found");
+
+    const posts = await ctx.db
+      .query("posts")
+      .withIndex("by_user", (q) => q.eq("userId", args.userId || user._id))
+      .collect();
+
+    return posts;
   },
 });
